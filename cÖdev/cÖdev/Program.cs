4baa3e084f38ef052e71f9b71a160cfe6c1dff20
zaferavci1 <avcizafer963@gmail.com>
2021-11-05 15:54:00 +0300

    05-11-2021

---
+++ cÖdev/cÖdev/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+
+namespace enginDemiroğ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Customer customer = new Customer();
+            customer.FirstName = "Musa";
+            customer.LastName = "   Uyumaz";
+            Kayit(customer);
+            Duzenle(customer);
+
+        }
+        static void Kayit(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + customer.LastName + "  Kullanıcı sisteme eklendi!");
+        }
+        static void Duzenle(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + "  Kullanıcının kaydı düzenlendi!");
+        }
+
+    }
+    class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+
+    }
+
+
+
+}
